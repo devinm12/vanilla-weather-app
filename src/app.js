4363d9d2c04db7df35b06f58691f545e545de533
devinm12 <devinschn12@gmail.com>
2023-02-05 13:26:36 -0600

    added search engine

--- src/app.js
+++ src/app.js
@@ -40,10 +40,21 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "t88f4d38b82a7fd38bfe80o03b85f024";
-let units = "imperial";
-let city = "Dallas";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
-console.log(apiUrl);
+function search(city) {
+  let apiKey = "t88f4d38b82a7fd38bfe80o03b85f024";
+  let units = "imperial";
 
-axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+
+  search(cityInputElement.value);
+}
+
+search("Dallas");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
